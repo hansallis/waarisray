1238af606e9089ad1bf8da8cda4b199f0b7db3fb
Hans Allis <hans@llis.nl>
2025-10-28 19:29:11 +0100

    Preparing for v8

---
+++ src/Evergreen/V8/Types.elm
@@ -0,0 +1,144 @@
+module Evergreen.V8.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Dict
+import Http
+import Lamdera
+import Time
+import Url
+
+
+type alias TelegramUser =
+    { id : Int
+    , firstName : String
+    , lastName : Maybe String
+    , username : Maybe String
+    , photoUrl : Maybe String
+    }
+
+
+type alias User =
+    { telegramUser : TelegramUser
+    , isRay : Bool
+    }
+
+
+type Page
+    = LoginPage
+    | GamePage
+    | ResultsPage String
+    | HistoryPage
+
+
+type alias Location =
+    { lat : Float
+    , lng : Float
+    }
+
+
+type alias Guess =
+    { userId : Int
+    , userName : String
+    , location : Location
+    , timestamp : Time.Posix
+    , distanceKm : Maybe Float
+    }
+
+
+type alias Round actualLocationType guessType =
+    { id : String
+    , actualLocation : actualLocationType
+    , startTime : Time.Posix
+    , endTime : Maybe Time.Posix
+    , guesses : Dict.Dict Int guessType
+    , isOpen : Bool
+    }
+
+
+type alias UncensoredRound =
+    Round Location Guess
+
+
+type alias CensoredRound =
+    Round () ()
+
+
+type FrontendRound
+    = Uncensored UncensoredRound
+    | Censored CensoredRound
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , currentUser : Maybe User
+    , page : Page
+    , currentRound : Maybe FrontendRound
+    , pastRounds : List UncensoredRound
+    , userGuess : Maybe Location
+    , pendingLocation : Maybe Location
+    , mapCenter : Location
+    , mapZoom : Int
+    , error : Maybe String
+    , authData : Maybe String
+    }
+
+
+type alias BackendModel =
+    { users : Dict.Dict Int User
+    , rounds : Dict.Dict String UncensoredRound
+    , currentRoundId : Maybe String
+    , userSessions : Dict.Dict Lamdera.SessionId Int
+    , failedAuthentications : List String
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | InitiateTelegramAuth
+    | TelegramAuthResult (Result Http.Error String)
+    | Logout
+    | LoginAsRay
+    | LoginAsRegularUser
+    | MapClicked Location
+    | StartNewRound
+    | ConfirmStartRound
+    | CancelPendingLocation
+    | SubmitGuess
+    | CloseRound
+    | ViewRound String
+    | GoToHistory
+    | GoToGame
+    | ClearError
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = AuthenticateWithTelegram String
+    | LogoutUser
+    | AuthenticateAsRay
+    | AuthenticateAsRegularUser
+    | CreateNewRound Location
+    | SubmitUserGuess Location
+    | EndCurrentRound
+    | RequestCurrentGameState
+    | RequestRoundHistory
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = AuthenticationResult (Result String User)
+    | GameStateUpdate
+        { currentUser : Maybe User
+        , currentRound : Maybe FrontendRound
+        , usersGuess : Maybe Location
+        , pastRounds : List UncensoredRound
+        }
+    | RoundCreated CensoredRound
+    | GuessSubmitted Guess
+    | RoundClosed UncensoredRound
+    | ErrorMessage String
